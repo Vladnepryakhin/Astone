d45e56b6213e3a4339745e529748b4b4e85f74fe
Vladnepryakhin <nv_slm@mail>
2025-04-22 19:26:18 +0400

    Yrok

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
-    public static void chekSumSing(int a, int b) {
-        if (a + b >= 0) {
-            System.out.println("Сумма положительная.");
-        } else {
-            System.out.println("Сумма отрицательная.");
-        }
-    }
     public static void main(String[] args) {
-        chekSumSing(5, 3);
-        chekSumSing(-10, 5);
+        Dog dogSharick = new Dog(" Шарик ");
+        Cat catMurzik = new Cat(" Мурзик ");
+        FoodBow1 foodBow1 = new FoodBow1(60);
+        dogSharick.run(150);
+        dogSharick.run(600);
+        dogSharick.swim(100);
+        dogSharick.swim(200);
+        catMurzik.run(150);
+        catMurzik.run(600);
+        catMurzik.swim(100);
+        foodBow1.feed(dogSharick, 50);
+        foodBow1.feed(catMurzik, 20);
+        foodBow1.addFood(50);
+        foodBow1.feed(catMurzik, 20);
+        System.out.println("Всего животных: " + Animals.getTotalAnimals());
+        System.out.println(" Всего псов: " + Animals.getTotalDogs());
+        System.out.println( " Всего кисонек: " + Animals.getTotalCats());
+
     }
 }
