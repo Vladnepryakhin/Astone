eaba054d0159ff410eff5db7d976249b21b67acc
Vladnepryakhin <nv_slm@mail>
2025-05-19 13:28:11 +0400

    Lessone_12

--- src/Figyri.java
+++
@@ -1,4 +0,0 @@
-public interface Figyri {
-    double calculatePerimeter();
-    double calculateArea();
-}
---
+++ src/test/java/ArithmeticOperationsTest.java
@@ -0,0 +1,35 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArithmeticOperationsTest {
+    private main.java.org.example.ArithmeticOperations ArithmeticOperations;
+
+    @Test
+    void testAddition() {
+        assertEquals(15, ArithmeticOperations.add(10, 5));
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(5, ArithmeticOperations.subtract(10, 5));
+    }
+
+    @Test
+    void testMultiplication() {
+        assertEquals(50, ArithmeticOperations.multiply(10, 5));
+    }
+
+    @Test
+    void testDivision() {
+        assertEquals(2.0, ArithmeticOperations.divide(10, 5));
+    }
+
+    @Test
+    void testDivisionByZero() {
+        assertThrows(ArithmeticException.class, () -> {
+            ArithmeticOperations.divide(10, 0);
+        });
+    }
+}
